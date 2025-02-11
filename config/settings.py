20e04e721c66488bca2f5f286a7920351312e7b4
AndyTums <tumerrkin@gmail.com>
2025-02-11 23:34:07 +0300

    fixe ci v72

--- config/settings.py
+++ config/settings.py
@@ -90,14 +90,7 @@ SIMPLE_JWT = {
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-# if 'test' in sys.argv:
-#     DATABASES = {
-#         'default': {
-#             'ENGINE': 'django.db.backends.sqlite3',
-#             'NAME': BASE_DIR / 'test.db.sqlite3',
-#         }
-#     }
-# else:
+
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
